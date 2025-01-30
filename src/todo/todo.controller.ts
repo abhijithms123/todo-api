befd1d280f233ebdbab959513c6686f77b9b9e14
Abhijith M S <abhijith.ms@bytestrone.com>
2025-01-30 15:25:50 +0530

    last commit

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { TodoResponse } from "./todo.dto";
 import { todos } from "./todo.mock";
@@ -19,4 +19,25 @@ export class TodoController {
     async getTodoById(@Param('id') id: string): Promise<TodoResponse> {
          return todos.find((todo) => todo.id === id);
 }
+
+  @Post('/add')
+  @ApiOperation({summary: 'Add new todo'})
+  @ApiOkResponse({description: 'New todo',type: TodoResponse})
+  async createTodo(@Body() todo: TodoResponse): Promise<TodoResponse> {
+    todos.push(todo);
+    return todos.find((data)=> data.id === todo.id);
+}
+
+// @Put('/edit')
+// @ApiOperation({summary: 'edit an existing todo'})
+// @ApiOkResponse({description: 'updated todo',type: TodoResponse})
+// async updateTodo(@Body() todo: TodoResponse): Promise<TodoResponse> {
+//    let currentTodo = todos.find((data)=>data.id === todo.id);
+//    todos.
+//    currentTodo.title = todo.title;
+//    currentTodo.description = todo.description;
+//    currentTodo.isCompleted = todo.isCompleted;
+//    return currentTodo;
+// }
+   
 }
--- src/todo/todo.mock.ts
+++ src/todo/todo.mock.ts
@@ -1,6 +1,15 @@
 export const todos = [
-    {id: '1', title: 'Todo 1', description: 'Description 1', isCompleted: false},
-    {id: '2', title: 'Todo 2', description: 'Description 2', isCompleted: false},
-    {id: '3', title: 'Todo 3', description: 'Description 3', isCompleted: false},
-    {id: '4', title: 'Todo 4', description: 'Description 4', isCompleted: false},
+    {id: '1', title: 'book plane ticket', description: 'Description 1', isCompleted: false},
+    {id: '2', title: 'go to the grocery store', description: 'Description 2', isCompleted: false},
+    {id: '3', title: 'pick up jason from the airport', description: 'Description 3', isCompleted: false},
+    {id: '4', title: 'take the dog for a walk and take it to the vet', description: 'Description 4', isCompleted: false},
+    {id: '5', title: 'book plane ticket', description: 'Description 1', isCompleted: false},
+    {id: '6', title: 'go to the grocery store', description: 'Description 2', isCompleted: false},
+    {id: '7', title: 'pick up jason from the airport', description: 'Description 3', isCompleted: false},
+    {id: '8', title: 'take the dog for a walk and take it to the vet', description: 'Description 4', isCompleted: false},
+    {id: '9', title: 'book plane ticket', description: 'Description 1', isCompleted: false},
+    {id: '10', title: 'go to the grocery store', description: 'Description 2', isCompleted: false},
+    {id: '11', title: 'pick up jason from the airport', description: 'Description 3', isCompleted: false},
 ]
+
+
